6b65bd05b97664d47d04c10773f0aeded9cb04d8
Nícholas André <nicholas@iotecnologia.com.br>
2015-04-02 19:07:56 -0300

    Full-width image and some fixes on navigation menu

--- footer.php
+++ footer.php
@@ -9,14 +9,14 @@
  */
 ?>
 
-		</div><!-- #main -->
 
-		<footer id="footer" role="contentinfo">
-			<div class="site-info">
-				<span>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'horizon-theme' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Horizon Theme</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'horizon-theme' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></span>
-			</div><!-- .site-info -->
-		</footer><!-- #footer -->
-	</div><!-- .container -->
+
+	<footer id="footer" role="contentinfo">
+		<div class="site-info">
+			<span>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'horizon-theme' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Horizon Theme</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'horizon-theme' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></span>
+		</div><!-- .site-info -->
+	</footer><!-- #footer -->
+
 
 	<?php wp_footer(); ?>
 </body>

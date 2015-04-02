9b08702b5f6fa9008487515d57596b1db980de66
Claudio Sanches <contato@claudiosmweb.com>
2015-04-02 19:12:59 -0300

    Added i18n tools #5

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,6 +102,53 @@ module.exports = function( grunt ) {
 					dest: '<%= dirs.images %>/'
 				}]
 			}
+		},
+
+		// Create .pot files
+		makepot: {
+			dist: {
+				options: {
+					type: 'wp-theme'
+				}
+			}
+		},
+
+		// Check Text Domain
+		checktextdomain: {
+			options:{
+				text_domain: '<%= pkg.name %>',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php', // Include all files
+					'!node_modules/**' // Exclude node_modules/
+				],
+				expand: true
+			}
+		},
+
+		// Convert .po files to .mo
+		po2mo: {
+			files: {
+				src: 'languages/*.po',
+				expand: true
+			}
 		}
 	});
 

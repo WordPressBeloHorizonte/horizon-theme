86033967b6f16f0375ad5d310ea1603677c7e0f4
Rodrigo Brito <rig.10@hotmail.com>
2015-04-12 12:15:34 -0300

    Include portfolio area

--- index.php
+++ index.php
@@ -149,6 +149,136 @@ get_header(); ?>
 			</div> <!-- .container-fluid -->
 		</section> <!-- #joker-sep -->
 
+		<section id="services">
+			<div class="container">
+
+				<header class="horizon-header">
+					<h2 class="title"><span>Horizon</span> Portfolio</h2>
+					<span class="sep"></span>
+					<p class="desc">Our work is your satisfaction</p>
+				</header>
+
+				<blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget nulla ex. Curabitur mauris felis, vestibulum eget eros ac,
+					congue suscipit felis. Etiam accumsan, libero ac tristique maximus, diam risus interdum odio, sed ultricies arcu quam vel mauris.</p>
+				</blockquote>
+
+			</div>
+		</section> <!-- #services -->
+
+		<section class="portfolio">
+			<div class="content">
+				<nav class="menu">
+					<ul>
+						<li class="menu-item">
+							<a href="#" class="menu-item-link">caterogia 01</a>
+						</li>
+						<li class="menu-item">
+							<a href="#" class="menu-item-link">caterogia 02</a>
+						</li>
+						<li class="menu-item">
+							<a href="#" class="menu-item-link">caterogia 03</a>
+						</li>
+						<li class="menu-item">
+							<a href="#" class="menu-item-link">caterogia 04</a>
+						</li>
+						<li class="menu-item">
+							<a href="#" class="menu-item-link">caterogia 05</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div class="portfolio-list clearfix">
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto3.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto4.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto5.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto6.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto7.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto8.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto3.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto4.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+				<div class="portfolio-item col-lg-3 col-md-4 col-sm-6">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/portifolio/foto5.jpg" alt="">
+					<div class="portfolio-item-caption">
+						<h4 class="portfolio-item-title">Lorem Ipsum</h4>
+						<span class="portfolio-item-description">Lorem Ipsum</span>    
+					</div>    
+				</div>
+			</div>
+			<div class="content load-more">
+				<button href="#" class="horizon-btn secondary"><?php _e( 'Load More', 'horizon-theme' ); ?></button>
+			</div>
+		</section>
+
 		<section id="clients">
 			<div class="container">
 				<header class="horizon-header">

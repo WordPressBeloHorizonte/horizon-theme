2245d28d11e26a6bf8c8001179576df8ca0cf464
Breno Alves <breno.alvs@hotmail.com>
2015-04-12 19:18:31 -0300

    Added icons to services area

--- index.php
+++ index.php
@@ -84,6 +84,81 @@ get_header(); ?>
 					congue suscipit felis. Etiam accumsan, libero ac tristique maximus, diam risus interdum odio, sed ultricies arcu quam vel mauris.</p>
 				</blockquote>
 
+				<div class="row">
+					<div class="service col-md-4 col-sm-6 col-xs-12">
+						<div class="service-wrapper">
+							<span class="service-icon fa fa-mobile"></span>
+							<h4>Fábrica de Aplicativos</h4>
+							<span class="sep"></span>
+							<p>Lorem ipsum dolor sit amet, consectetur.</p>
+						</div>
+					</div>
+					<div class="service col-md-4 col-sm-6 col-xs-12">
+						<div class="service-wrapper">
+							<span class="service-icon fa fa-desktop"></span>
+							<h4>Web Design</h4>
+							<span class="sep"></span>
+							<p>Lorem ipsum dolor sit amet, consectetur.</p>
+						</div>
+					</div>
+					<div class="service col-md-4 col-sm-6 col-xs-12">
+						<div class="service-wrapper">
+							<span class="service-icon fa fa-paint-brush"></span>
+							<h4>Design Gráfico</h4>
+							<span class="sep"></span>
+							<p>Lorem ipsum dolor sit amet, consectetur.</p>
+						</div>
+					</div>
+					<div class="service col-md-4 col-sm-6 col-xs-12">
+						<div class="service-wrapper">
+							<span class="service-icon dashicons dashicons-groups"></span>
+							<h4>Redes Sociais</h4>
+							<span class="sep"></span>
+							<p>Lorem ipsum dolor sit amet, consectetur.</p>
+						</div>
+					</div>
+					<div class="service col-md-4 col-sm-6 col-xs-12">
+						<div class="service-wrapper">
+							<span class="service-icon fa fa-bar-chart"></span>
+							<h4>Gestão de Projetos</h4>
+							<span class="sep"></span>
+							<p>Lorem ipsum dolor sit amet, consectetur.</p>
+						</div>
+					</div>
+					<div class="service col-md-4 col-sm-6 col-xs-12">
+						<div class="service-wrapper">
+							<span class="service-icon dashicons dashicons-lightbulb"></span>
+							<h4>Branding</h4>
+							<span class="sep"></span>
+							<p>Lorem ipsum dolor sit amet, consectetur.</p>
+						</div>
+					</div>
+					<div class="service col-md-4 col-sm-6 col-xs-12">
+						<div class="service-wrapper">
+							<span class="service-icon fa fa-mobile"></span>
+							<h4>Fábrica de Aplicativos</h4>
+							<span class="sep"></span>
+							<p>Lorem ipsum dolor sit amet, consectetur.</p>
+						</div>
+					</div>
+					<div class="service col-md-4 col-sm-6 col-xs-12">
+						<div class="service-wrapper">
+							<span class="service-icon fa fa-desktop"></span>
+							<h4>Web Design</h4>
+							<span class="sep"></span>
+							<p>Lorem ipsum dolor sit amet, consectetur.</p>
+						</div>
+					</div>
+					<div class="service col-md-4 col-sm-6 col-xs-12">
+						<div class="service-wrapper">
+							<span class="service-icon fa fa-paint-brush"></span>
+							<h4>Design Gráfico</h4>
+							<span class="sep"></span>
+							<p>Lorem ipsum dolor sit amet, consectetur.</p>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</section> <!-- #services -->
 

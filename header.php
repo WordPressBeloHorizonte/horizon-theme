2d4b46f032cf10d0a2cb900c169fbbc5ab21a161
Sergio Costa <Sergio@Sergios-MacBook-Pro.local>
2015-04-23 22:49:43 -0300

    title tag added to header.php

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" rel="shortcut icon" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
 
 	<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5.js"></script>
